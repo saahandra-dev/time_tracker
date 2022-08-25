49d69e8b90dd738da2ffac48888341bcb8d9664b
saahandraa <sandraemenikee@gmail.com>
2022-08-25 19:59:41 +0100

    edit and update_funct

--- App.js
+++ App.js
@@ -1,31 +1,78 @@
 import React from 'react';
 import { StyleSheet, View, ScrollView, Text } from 'react-native';
+import uuid from 'react-native-uuid';
+
 import EditableTimer from './components/EditableTimer';
 import ToggleableTimerForm from './components/ToggleableTimerForm';
+import { newTimer } from './utils/TimerUtils';
 
 export default class App extends React.Component {
+  state = {
+    timers: [
+      {
+        title: 'Mow the lawn',
+        project: 'House Chores',
+        id: uuid.v4(),
+        elapsed: 5456099,
+        isRunning: true,
+      },
+      {
+        title: 'Bake squash',
+        project: 'Kitchen Chores',
+        id: uuid.v4(),
+        elapsed: 1273998,
+        isRunning: false,
+      }
+    ]
+  }
+  handleCreatFormSubmit = timer => {
+    const { timers } = this.state;
+
+    this.setState({
+      timers: [newTimer(timer), ...timers],
+    });
+  }
+  handleFormSubmit = attrs => {
+    const { timers } = this.state;
+
+    this.setState({
+      timers: timers.map(timer => {
+        if (timer.id === attrs.id) {
+          const { title, project } = attrs;
+
+          return {
+            ...timer,
+            title,
+            project,
+          };
+        }
+        return timer;
+      }),
+    });
+  };
   render() {
+    const { timers } = this.state;
+
     return (
       <View style={styles.appContainer}>
         <View style={styles.titleContainer}>
           <Text style={styles.title}>Timers</Text>
         </View>
         <ScrollView style={styles.timerList}>
-          <ToggleableTimerForm isOpen={false} />
-          <EditableTimer
-            id="1"
-            title="Mow the lawn"
-            project="House Chores"
-            elapsed="8986300"
-            isRunning
+          <ToggleableTimerForm 
+            onFormSubmit={this.handleCreatFormSubmit}
           />
-          <EditableTimer
-            id="2"
-            title="Bake squash"
-            project="Kitchen Chores"
-            elapsed="3890985"
-            // editFormOpen
-          />
+          {timers.map(({ title, project, id, elapsed, isRunning }) => (
+            <EditableTimer
+              key={id}
+              id={id}
+              title={title}
+              project={project}
+              elapsed={elapsed}
+              isRunning={isRunning}
+              onFormSubmit={this.handleFormSubmit}
+            />
+          ))}
         </ScrollView>
       </View>
     )
@@ -39,7 +86,8 @@ const styles = StyleSheet.create({
   titleContainer: {
     paddingTop: 35,
     paddingBottom: 15,
-    borderBottomColor: 'D6D7DA',
+    borderBottomWidth: 1,
+    borderBottomColor: '#D6D7DA',
   },
   title: {
     fontSize: 18,
